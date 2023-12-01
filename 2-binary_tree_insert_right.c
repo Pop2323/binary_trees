59a589a02d6bdf3c154b1a44b3c22439de6c3d9b
Pop2323 <omar4500455@gmail.com>
2023-12-01 09:51:29 -0800

    binary_trees

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -26,7 +26,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (!new_node)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
