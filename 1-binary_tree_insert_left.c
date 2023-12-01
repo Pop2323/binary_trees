e32b719f40dadb21868b11cc0413263a7908b8d8
Pop2323 <omar4500455@gmail.com>
2023-12-01 09:45:29 -0800

    binary_trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,15 +23,24 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
+
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->right = NULL;
-	new_node->left = parent->left;
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	else
+	{
+		new_node->left = NULL;
+	}
 	parent->left = new_node;
-	if (new_node)
-		new_node->left->parent = new_node;
+
 	return (new_node);
 }
